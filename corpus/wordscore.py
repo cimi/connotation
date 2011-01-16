e63c7ad1bcf3b072c0068afd2f046155c2ce6765
Alex Ciminian <cimi@ubuntu.(none)>
2011-01-16 14:29:17 +0200

    dummy score implementation

---
+++ corpus/wordscore.py
@@ -0,0 +1,5 @@
+from random import randint
+class WordScore():
+	def getScore(self, word):
+		"""Returns an integer representing the score associated to a word by SentiWordNet""" 
+		return randint(-5,5)
--- main.py
+++ main.py
@@ -7,8 +7,12 @@ class main:
     def main(self):
 	sources = ["The quick brown fox jumps over the lazy dog", "This is ninja stuff right there",
                "This is micky changing connotation project from eclipse:)"]
-	print sources
-	print self.member
+	# get the corpus interface
+	sys.path.append('corpus')
+	import wordscore
+	c = wordscore.WordScore()
+	word = 'coconut'
+	print c.getScore(word)
 
 
 if __name__ == '__main__':
