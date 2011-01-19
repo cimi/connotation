422c4f5896ef2c8bd782551e7c8b723daf27e616
Alex Ciminian <alex.ciminian@gmail.com>
2011-01-19 22:14:49 +0200

    small corrections, swn still does not match wn offsets

--- corpus/wordscore.py
+++ corpus/wordscore.py
@@ -17,10 +17,14 @@ class WordScore():
 		"""Returns an integer representing the score associated to a word by SentiWordNet""" 
 		# first, get the synset of the word from wordnet
 		synsets = wordnet.synsets(word)
+		print "The word " + word + "is associated with the following synsets:"			
 		res = []
 		for synset in synsets:
+			# print out synsets for debugging
+			print synset
+			print  "... with offset " + str(synset.offset) + " and pos " + synset.pos 
 			# get a list of all scores that match in swn
-			if self.swn.has_key((synsets[0].pos, synsets[0].offset)):
+			if self.swn.has_key((synset.pos, synset.offset)):
 				res.append(self.swn[(synset.pos, synset.offset)][0:1])
 		return res
 
