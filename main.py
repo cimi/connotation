b465c56402ac4ca47ef5f0dba29b4504aa72f590
Alex Ciminian <alex.ciminian@gmail.com>
2011-01-23 11:36:57 +0200

    Added pulling params from config file, commented out algorithm section because of missing file

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import sys
+import ConfigParser
 # from corpus.wordscore import WordScore
 from nltk.corpus import wordnet
-from logic.algorithm import Algorithm
+# from logic.algorithm import Algorithm
 class main: 
     def __init__(self):
 	self.member = "token"        
 
     def main(self):
+	config = ConfigParser.ConfigParser()
+	config.read('settings.cfg')
+	print config.get('Algorithm', 'name')
+	print config.get('Data', 'name')
+	print config.get('Corpus', 'name')
+	print config.get('Corpus','file')
+	
 	sources = ["The quick brown fox jumps over the lazy dog", "This is ninja stuff right there",
                "This is micky changing connotation project from eclipse:)",
                "The girl has an apple"]
+	
+	
 	# get the corpus interface
-	sys.path.append('corpus')
 	# c = WordScore('corpus/data/swn.txt')
 	# get the score of the first word given as a command line argument
 	# print c.getScore(sys.argv[1])
-	
+	quit()	
 	a = Algorithm();
 	for text in sources:
 		print "<<" + text + ">> has score " + str(a.getScore(text)) + "\n\n\n"
